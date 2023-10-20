49276a836e379a6e035efc500cb0889f006e7ff3
nicol <nicol.tapia@cloud.uautonoma.cl>
2023-10-20 15:34:39 -0300

    Corregir el codigo dentro del fragment

--- app/src/main/java/com/example/casino/BarraNav/MenuFragment.kt
+++ app/src/main/java/com/example/casino/BarraNav/MenuFragment.kt
@@ -1,10 +1,13 @@
-package com.example.ejemplosmenu.Fragment
+package com.example.casino.BarraNav
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.casino.Menus.MenuBebidas
 import com.example.casino.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,10 +17,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SettingFragment.newInstance] factory method to
+ * Use the [MenuFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingFragment : Fragment() {
+class MenuFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +38,13 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_conf, container, false)
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+
+        //ACa debe ir la logica del fragment
+
+
+
+
     }
 
     companion object {
@@ -45,16 +54,17 @@ class SettingFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingFragment.
+         * @return A new instance of fragment MenuFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SettingFragment().apply {
+            MenuFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
+
 }
--- app/src/main/java/com/example/casino/Menu.kt
+++ app/src/main/java/com/example/casino/Menu.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
+import com.example.casino.BarraNav.MenuFragment
 import com.example.casino.Menus.MenuAlmuerzo
 import com.example.casino.Menus.MenuBebidas
 import com.example.casino.Menus.MenuComidaRapida
@@ -14,8 +15,7 @@ import com.example.casino.Menus.MenuPostres
 import com.example.casino.Menus.MenuSandwich
 import com.example.casino.Menus.MenuTeCafe
 import com.example.casino.databinding.ActivityMenuBinding
-import com.example.ejemplosmenu.Fragment.HomeFragment
-import com.example.ejemplosmenu.Fragment.SettingFragment
+
 
 class Menu : AppCompatActivity() {
     private lateinit var menuBebidaButton: AppCompatButton
@@ -33,57 +33,16 @@ class Menu : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        menuBebidaButton = findViewById(R.id.menubebida)
-        menuBebidaButton.setOnClickListener {
-            val intent = Intent(this, MenuBebidas::class.java)
-            startActivity(intent)
-        }
-        menupostre = findViewById(R.id.menupostre)
-        menupostre.setOnClickListener {
-            val intent = Intent(this, MenuPostres::class.java)
-            startActivity(intent)
-        }
-        menualmuerzos = findViewById(R.id.menualmuerzos)
-        menualmuerzos.setOnClickListener {
-            val intent = Intent(this, MenuAlmuerzo::class.java)
-            startActivity(intent)
-        }
-        menufrutas = findViewById(R.id.menufrutas)
-        menufrutas.setOnClickListener {
-            val intent = Intent(this, MenuFrutas::class.java)
-            startActivity(intent)
-        }
-        menucomidarapida = findViewById(R.id.menucomidarapida)
-        menucomidarapida.setOnClickListener {
-            val intent = Intent(this, MenuComidaRapida::class.java)
-            startActivity(intent)
-        }
-        menugalletas = findViewById(R.id.menugalletas)
-        menugalletas.setOnClickListener {
-            val intent = Intent(this, MenuGalletas::class.java)
-            startActivity(intent)
-        }
-        menusandwich = findViewById(R.id.menusandwich)
-        menusandwich.setOnClickListener {
-            val intent = Intent(this, MenuSandwich::class.java)
-            startActivity(intent)
-        }
-        menutecafe = findViewById(R.id.menutecafe)
-        menutecafe.setOnClickListener {
-            val intent = Intent(this, MenuTeCafe::class.java)
-            startActivity(intent)
-        }
 
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //reemplaza el frame x home
-        replaceFragment(HomeFragment())
+        replaceFragment(MenuFragment())
 
         binding.bottomNavigation.setOnItemSelectedListener{
             when(it.itemId) {
-                R.id.nav_home -> replaceFragment(HomeFragment())
-                R.id.nav_setting -> replaceFragment(SettingFragment())
+                R.id.nav_home -> replaceFragment(MenuFragment())
                 else -> {}
             }
             true
@@ -92,9 +51,8 @@ class Menu : AppCompatActivity() {
     private fun replaceFragment(Fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, Fragment)
+        fragmentTransaction.replace(R.id.framelayout, Fragment)
 
         fragmentTransaction.commit()
-    }
     }
 }
