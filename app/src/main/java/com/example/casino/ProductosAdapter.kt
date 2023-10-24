b85716a9bd883f1bff08fd9ae3d9f7a5e958aba0
nicol <nicol.tapia@cloud.uautonoma.cl>
2023-10-24 13:06:41 -0300

    menu funcional agrega productos a la bd funcional mostrar producto en el recicle view funcional

--- app/src/main/java/com/example/casino/ProductosAdapter.kt
+++ app/src/main/java/com/example/casino/ProductosAdapter.kt
@@ -1,26 +1,16 @@
 package com.example.casino
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.casino.Productos
-import com.example.casino.R
 
 class ProductosAdapter(
     private val productos: MutableList<Productos>
 ) : RecyclerView.Adapter<ProductosAdapter.ProductoViewHolder>() {
-
-    // Listener para manejar la selección de categoría
-    private var onCategoriaSelectedListener: ((Int, String) -> Unit)? = null
-
-    // Método para establecer el listener
-    fun setOnCategoriaSelectedListener(listener: (Int, String) -> Unit) {
-        onCategoriaSelectedListener = listener
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductoViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -31,21 +21,12 @@ class ProductosAdapter(
     override fun onBindViewHolder(holder: ProductoViewHolder, position: Int) {
         val producto = productos[position]
 
-        holder.spinnerCategory.setSelection(getCategoriaPosition(producto.categoria))
-
-        // Maneja la selección de categoría en el Spinner
-        holder.spinnerCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                onCategoriaSelectedListener?.invoke(holder.adapterPosition, parent?.getItemAtPosition(position).toString())
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-        }
+        // Asigna los datos del producto a las vistas en el diseño del elemento
+        holder.nombreProducto.text = producto.nombre
+        holder.descripcion.text = producto.descripcion
+        holder.precio.text = "Precio: $" + producto.precio.toString()
+        holder.cantidad.text = "Cantidad: " + producto.cantidad.toString()
+        holder.categoria.text = "Categoría: " + producto.categoria
     }
 
     override fun getItemCount(): Int {
@@ -53,12 +34,10 @@ class ProductosAdapter(
     }
 
     inner class ProductoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val spinnerCategory: Spinner = itemView.findViewById(R.id.categoria)
-    }
-
-    // Busca la posición de la categoría seleccionada en el Spinner
-    private fun getCategoriaPosition(categoria: String): Int {
-        val categorias = arrayOf("Bebidas", "Postres", "Almuerzos", "Frutas", "Comida Rápida", "Galletas", "Sandwich", "Té/Café")
-        return categorias.indexOf(categoria)
+        val nombreProducto: TextView = itemView.findViewById(R.id.nombre_producto)
+        val descripcion: TextView = itemView.findViewById(R.id.descripcion)
+        val precio: TextView = itemView.findViewById(R.id.precio)
+        val cantidad: TextView = itemView.findViewById(R.id.cantidad)
+        val categoria: TextView = itemView.findViewById(R.id.categoria)
     }
 }
